af404165ed6b2cae5d0e84d944aa2871931adc7c
joaovbmdias <github.stepgrandmother@aleeas.com>
2022-07-07 17:01:54 +0000

    Update time.js

--- assets/js/time.js
+++ assets/js/time.js
@@ -4,10 +4,28 @@
 // Set time and Date
 
 window.onload = displayClock();
+
+function changeTimeZone(date, timeZone) {
+  if (typeof date === 'string') {
+    return new Date(
+      new Date(date).toLocaleString('en-GB', {
+        timeZone,
+      }),
+    );
+  }
+
+  return new Date(
+    date.toLocaleString('en-GB', {
+      timeZone,
+    }),
+  );
+}
+
 function displayClock() {
 	const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 	var d = new Date();
+	d = changeTimeZone(d, 'Europe/London');
 	var mm = monthNames[d.getMonth()];
 	var dd = d.getDate();
 	var min = (mins = ('0' + d.getMinutes()).slice(-2));
@@ -20,7 +38,7 @@ function displayClock() {
 		hh = hh ? hh : 12;
 	}
 
-	document.getElementById('hour').innerText = hh + 1;
+	document.getElementById('hour').innerText = hh;
 	document.getElementById('separator').innerHTML = ' : ';
 	document.getElementById('minutes').innerText = min + ampm;
 
